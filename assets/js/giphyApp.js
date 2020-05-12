b84dc4755c581f316fcd12f1b017caa9a9eacd6e
Noe Leiva <leiva.noe.86@gmail.com>
2020-05-12 17:47:55 -0500

    created giphy call function. Also dynamic button creation

--- assets/js/giphyApp.js
+++ assets/js/giphyApp.js
@@ -1,17 +1,42 @@
 
 
 // Used to create the buttons 
-var giphyCategories = ["dog","cat","hamster","bird","dolphin"]
+var giphyCategories = ["DOG","CAT","HAMSTER","BIRD","DOLPHIN"]
+
+// Here's where the buttons will be loaded
+var buttonContainer = $("#button_container");
 
 
 // Create Buttons
 function createButtons (){
 
+    for(var i = 0; i < giphyCategories.length; i++){
+        var categoryButton = $("<button>");
+        $(categoryButton).attr("type","button");
+        $(categoryButton).attr("class","btn btn-info");
+        $(categoryButton).text(giphyCategories[i]);
+        $(buttonContainer).append(categoryButton);
+    }
+
+}
+
+function giphyCall (giphyCategory){
+
 }
 
 
-// 
+$(document).on("click", ".btn-info") {
+    var categoryName = $(this).text();
+    console.log(categoryName)
+    
+}
+
+
+// Start App
 $(document).ready(function(){
+
+    // first create buttons
+    createButtons();
 
 })
 
