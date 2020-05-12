7341607bab8ffc76a5520dc33541774cc2f118bf
Noe Leiva <leiva.noe.86@gmail.com>
2020-05-12 16:54:36 -0500

    added main container for the giphy and buttons in the index html. In addition, created the ready function and create buttons function in giphy app js

---
+++ assets/js/giphyApp.js
@@ -0,0 +1,19 @@
+
+
+// Used to create the buttons 
+var giphyCategories = ["dog","cat","hamster","bird","dolphin"]
+
+
+// Create Buttons
+function createButtons (){
+
+}
+
+
+// 
+$(document).ready(function(){
+
+})
+
+
+
