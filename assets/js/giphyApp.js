1cc35965ab3eadb507b7d5536d53bb9e9e1fb71b
Noe Leiva <leiva.noe.86@gmail.com>
2020-05-12 19:54:38 -0500

    created random category function and giphy call function. In addition, added event handler to stop the image. Finally added the still image and animate image to each of the images loaded

--- assets/js/giphyApp.js
+++ assets/js/giphyApp.js
@@ -6,6 +6,16 @@ var giphyCategories = ["DOG","CAT","HAMSTER","BIRD","DOLPHIN"]
 // Here's where the buttons will be loaded
 var buttonContainer = $("#button_container");
 
+// Used to load giphy
+var giphyContainer = $("#giphy_container");
+
+// Giphy URL
+var queryURL = "https://api.giphy.com/v1/gifs/";
+
+// Api Query
+var apiKey = "api_key=yfswyasYEAhn6HkkdSPoOt4vKr3KKGUF";
+
+
 
 // Create Buttons
 function createButtons (){
@@ -20,23 +30,65 @@ function createButtons (){
 
 }
 
-function giphyCall (giphyCategory){
-
-}
-
-
-$(document).on("click", ".btn-info") {
-    var categoryName = $(this).text();
-    console.log(categoryName)
+function loadGifs(gifData){
     
+    var results = gifData.data;
+
+    for(var i = 0; i < results.length; i++){
+        var image = $("<img>");
+        var imageUrl = results[i].images.fixed_height.url;
+        // console.log(imageurl);
+        $(image).attr("src", imageUrl);
+        $(image).addClass("gif_grp")
+
+        $(giphyContainer).append(image);
+    }
+
 }
 
+
+function giphyCall (giphyCategory){
+    
+    $(giphyContainer).empty();
+    
+    var searchCategory = "search?q=" + giphyCategory;
+
+    $.ajax({
+        url: queryURL + searchCategory + "&" + apiKey ,
+        method: "GET"
+    })
+    .then(function(response){
+        loadGifs(response);
+    });
+}
+
+
+function randomCategory(categoryArray){
+    return categoryArray[Math.floor(Math.random() * categoryArray.length)];
+}
+
+
+$(document).on("click",".btn-info", function(){
+
+    var categoryName = $(this).text();
+
+    giphyCall(categoryName);
+
+});
+
+$(document).on("click",".gif_grp", function(){
+    
+})
 
 // Start App
 $(document).ready(function(){
 
     // first create buttons
     createButtons();
+
+    var category = randomCategory(giphyCategories);
+
+    giphyCall(category)
 
 })
 
